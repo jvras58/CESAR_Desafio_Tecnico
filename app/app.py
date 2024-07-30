c79786206ac3616fb2d9c0b7a32bb6d6565b0949
Jonathas Vinicius <jonathas.vinicius@ufpe.br>
2024-07-30 05:05:51 -0300

    feat: add conexão com o nosso banco no streamlit

--- app/app.py
+++ app/app.py
@@ -1,10 +1,25 @@
 """Dashboard do CESAR - Desafio Técnico."""
 
 import streamlit as st
+from models.projects import Projects
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 # Título do dashboard
 st.title('Dashboard do CESAR - Desafio Técnico')
 
-st.markdown("""
-    # este é um dashboard interativo para *visualização de dados*.
-""")
+engine = create_engine('sqlite:///../database.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+def get_projects()-> list:
+    """Retorna uma lista com todos os projetos."""
+    return session.query(Projects).all()
+
+projects = get_projects()
+project_names = [project.project_name for project in projects]
+selected_project = st.selectbox('Selecione um projeto', project_names)
+st.write(f'Você selecionou: {selected_project}')
+
+
