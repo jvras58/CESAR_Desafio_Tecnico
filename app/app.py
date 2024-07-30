1ec0c8e208a1e5b8c9ab888f014463cbcf41365d
jvras <jonathas.vincius@ufpe.br>
2024-07-30 13:30:59 +0000

    feat: Adicionado grafico de rentabilidade líquida (receita - custo)

--- app/app.py
+++ app/app.py
@@ -1,10 +1,13 @@
 """Dashboard do CESAR - Desafio Técnico."""
 
-
 from __future__ import annotations
 
-import matplotlib.pyplot as plt
-import numpy as np
+from datetime import datetime, timedelta
+from pathlib import Path
+
+import pandas as pd
+import plotly.express as px
+import pytz
 import streamlit as st
 from models.projects import Projects
 from sqlalchemy import create_engine
@@ -17,8 +20,7 @@ engine = create_engine('sqlite:///../database.db')
 Session = sessionmaker(bind=engine)
 session = Session()
 
-
-def get_projects(limit:int | None = None) -> list:
+def get_projects(limit: int | None = None) -> list:
     """Retorna uma lista com todos os projetos.
 
     limitando a quantidade se especificado.
@@ -28,64 +30,71 @@ def get_projects(limit:int | None = None) -> list:
         query = query.limit(limit)
     return query.all()
 
+def get_project_ultimo_ano(projects: list) -> list:
+    """Filtra projetos realizados no último ano.
+
+    e calcula a rentabilidade líquida.
+    """
+    utc_now = datetime.now(pytz.utc)
+    one_year_ago = utc_now - timedelta(days=365)
+    return [
+        {
+            'nome': project.project_name,
+            'Rentabilidade_liquida': project.revenue - project.project_cost,
+        }
+        for project in projects
+        if project.end_date.replace(tzinfo=pytz.utc) >= one_year_ago
+    ]
 
 # Buscar dados dos projetos
-projects = get_projects(5)
+projects = get_projects(10)
 
-# Criar listas para os dados
-project_names = [project.project_name for project in projects]
-initial_budgets = [project.initial_budget for project in projects]
-project_costs = [project.project_cost for project in projects]
+st.write("Rentabilidade líquida de cada projeto realizado no último ano.")
+st.write(f"Total de projetos retornados pelo Banco: {len(projects)}")
 
-# Criar gráfico de barras duplas
-plt.figure(figsize=(10, 6))
-bar_width = 0.35
-index = range(len(project_names))
+# Filtra projetos do último ano e calcular a rentabilidade líquida
+project_ultimo_ano = get_project_ultimo_ano(projects)
+st.write(f"Total de projetos do último ano: {len(project_ultimo_ano)}")
 
-plt.bar(index, initial_budgets, bar_width, label='Orçamento Inicial')
-plt.bar([i + bar_width for i in index],
-        project_costs, bar_width,
-        label='Custo do Projeto')
+# Cria o gráfico de rentabilidade líquida
+if project_ultimo_ano:
+    projects_df = pd.DataFrame(project_ultimo_ano)
+    st.write("Dados dos projetos do último ano:")
+    st.write(projects_df)
+    fig = px.bar(
+        projects_df,
+        x='nome',
+        y='Rentabilidade_liquida',
+        title='Rentabilidade Líquida dos Projetos do Último Ano',
+        text='Rentabilidade_liquida',
+    )
 
-plt.xlabel('Nome do Projeto')
-plt.ylabel('Valor (R$)')
-plt.title('Comparação de Orçamento Inicial e Custo do Projeto')
-plt.xticks([i + bar_width / 2 for i in index], project_names, rotation=45)
-plt.legend()
+    # Atualizações de layout para melhorar a legibilidade
+    fig.update_layout(
+        xaxis_title='Nome do Projeto',
+        yaxis_title='Rentabilidade Líquida',
+        title='Rentabilidade Líquida dos Projetos do Último Ano',
+        xaxis_tickangle=-45,
+        font={'size': 14},
+    )
 
-# Mostrar gráfico no Streamlit
-st.pyplot(plt)
+    # Adicionando cores diferentes para cada barra
+    fig.update_traces(marker_color=px.colors.qualitative.Plotly,
+                    texttemplate='%{text:.2s}',
+                    textposition='outside')
+
+    # Exibi gráfico
+    st.plotly_chart(fig)
+else:
+    st.write("Nenhum projeto realizado no último ano.")
+
+# Le o conteúdo do arquivo SQL
+with Path('/workspace/scripts/rentabilidade_liquida.sql').open() as file:
+    rentabilidade_liquida_sql = file.read()
+
+# Exibi a consulta SQL abaixo do gráfico
+st.markdown("### Consulta SQL")
+st.code(rentabilidade_liquida_sql, language='sql')
 
 
 
-
-
-# Criar gerador de números aleatórios
-rng = np.random.default_rng(19680801)
-
-n_bins = 10
-x = rng.random((1000, 3))
-
-fig, ((ax0, ax1), (ax2, ax3)) = plt.subplots(nrows=2, ncols=2)
-
-colors = ['red', 'tan', 'lime']
-ax0.hist(x, n_bins, density=True, histtype='bar', color=colors, label=colors)
-ax0.legend(prop={'size': 10})
-ax0.set_title('bars with legend')
-
-ax1.hist(x, n_bins, density=True, histtype='bar', stacked=True)
-ax1.set_title('stacked bar')
-
-ax2.hist(x, n_bins, histtype='step', stacked=True, fill=False)
-ax2.set_title('stack step (unfilled)')
-
-# Fazer um histograma múltiplo de conjuntos de dados
-# com diferentes comprimentos.
-x_multi = [rng.random(n) for n in [10000, 5000, 2000]]
-ax3.hist(x_multi, n_bins, histtype='bar')
-ax3.set_title('different sample sizes')
-
-fig.tight_layout()
-
-# Mostrar gráfico no Streamlit
-st.pyplot(fig)
